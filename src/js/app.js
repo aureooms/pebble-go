533fcbdfaaa909059cd9ebc57dd1714a7664c881
Aurélien Ooms <aureooms@users.noreply.github.com>
2016-10-06 05:37:42 +0200

    freeze/unfreeze

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,55 @@
+// modules
+
 var UI = require('ui');
 var Vector2 = require('vector2');
 
 var ajax = require('ajax');
 var Vibe = require('ui/vibe');
+var Settings = require('settings');
 
-var _lock = false ;
+// colors
+
+var BOK = '#55AA55' ;
+var FOK = '#FFFFFF' ;
+var BLO = '#FFFF55' ;
+var FLO = '#000000' ;
+var BKO = '#FF0055' ;
+var FKO = '#FFFFFF' ;
+var BNG = '#FFAA00' ;
+var FNG = '#FFFFFF' ;
+var BG = '#FFFFFF' ;
+
+// locking
+
+var _LOCK = false ;
 
 function lock ( ) {
-	if ( _lock ) {
+	if ( _LOCK ) {
 		console.log( 'could not acquire lock' ) ;
 		return false ;
 	}
 	console.log( 'acquired lock' ) ;
-	_lock = true ;
+	_LOCK = true ;
 	return true ;
 }
 
 function release ( ) {
 	console.log( 'released lock' ) ;
-	_lock = false ;
+	_LOCK = false ;
 }
+
+// globals
 
 var GEOID = null ;
 var GEOERROR = 'GEOLOCATION NOT STARTED' ;
 var LAT = null ;
 var LON = null ;
+var DATA = null ;
 var TIMESTAMP = 0 ;
 var TKO = 60000 ;
 var STOP_ID = null ;
-var LINE_ID = null ;
-var STOP_INDEX = null ;
-var LINE_INDEX = null ;
+var STOP_INDEX = 0 ;
 var ERROR = null ;
-var DATA = null ;
 var MAX_REQUESTS = 10 ;
 var NCLOSEST = 10 ;
 var POLLRATE = 30000 ;
@@ -44,20 +61,42 @@ var NAVIGATOR_GEOLOCATION_OPTS = {
 
 var TIMEOUT = null;
 
-var BOK = '#55AA55' ;
-var FOK = '#FFFFFF' ;
-var BLO = '#FFFF55' ;
-var FLO = '#000000' ;
-var BKO = '#FF0055' ;
-var FKO = '#FFFFFF' ;
-var BNG = '#FFAA00' ;
-var FNG = '#FFFFFF' ;
+// (un)freeze
+
+var DEFAULT_STATE = {
+	LAT : LAT ,
+	LON : LON ,
+	DATA : DATA ,
+	STOP_ID : STOP_ID
+} ;
+
+function freeze ( ) {
+	console.log('freeze');
+	var state = {
+		LAT : LAT ,
+		LON : LON ,
+		DATA : DATA ,
+		STOP_ID : STOP_ID
+	} ;
+	Settings.data('state', state);
+}
+
+function unfreeze ( ) {
+	console.log('unfreeze');
+	var state = Settings.data('state') || DEFAULT_STATE ;
+	LAT = state.LAT ;
+	LON = state.LON ;
+	DATA = state.DATA ;
+	STOP_ID = state.STOP_ID ;
+}
+
+// display
 
 var WIDGETS = [];
 
-var main = new UI.Window({
+var MAIN = new UI.Window({
 	scrollable: true,
- 	backgroundColor: '#FFFFFF',
+ 	backgroundColor: BG,
 	status: {
 		separator : 'none',
 		color: FKO,
@@ -65,13 +104,14 @@ var main = new UI.Window({
 	}
 });
 
-var size = main.size();
-var w = size.x ;
-var h = size.y ;
+var SIZE = MAIN.size() ;
+var W = SIZE.x ;
+var H = SIZE.y ;
+var LEFT = 15;
 
-var fstop = new UI.Text({
+var FSTOP = new UI.Text({
  position: new Vector2(25, 0),
- size: new Vector2(w - 50, 20),
+ size: new Vector2(W - 50, 20),
  font: 'gothic-24-bold',
  backgroundColor: 'none',
  color: '#000000' ,
@@ -79,18 +119,18 @@ var fstop = new UI.Text({
  textOverflow: 'ellipsis'
 });
 
-var LEFT = 15;
-
-var fmessage = new UI.Text({
+var FMESSAGE = new UI.Text({
  position: new Vector2(LEFT, 30),
- size: new Vector2(w - 34, h - 30),
+ size: new Vector2(W - 34, H - 30),
  font: 'gothic-24-bold',
  color: BKO ,
  textAlign: 'center',
  textOverflow: 'wrap'
 });
 
-main.show();
+MAIN.show();
+
+// requests handling
 
 function api ( lat , lon ) {
 	var n = NCLOSEST ;
@@ -100,42 +140,42 @@ function api ( lat , lon ) {
 
 function ad ( f ) {
 	WIDGETS.push(f);
-	main.add(f);
+	MAIN.add(f);
 }
 
 function rm ( f ) {
 	f.text('');
-	main.remove(f);
+	MAIN.remove(f);
 }
 
 function clear ( ) {
 	var len = WIDGETS.length ;
-	for ( var i = 0 ; i < len ; ++i ) {
-		rm(WIDGETS[i]);
-	}
+	for ( var i = 0 ; i < len ; ++i ) rm(WIDGETS[i]);
 	WIDGETS = [] ;
 }
 
 function _display ( ) {
 	
+	console.log('display');
+	
 	clear();
 	
 	STOP_ID = DATA.stops[STOP_INDEX].id ;
 	
-	fstop.text( DATA.stops[STOP_INDEX].name );
-	ad(fstop);
+	FSTOP.text( DATA.stops[STOP_INDEX].name );
+	ad(FSTOP);
 	
 	if ( DATA.stops[STOP_INDEX].realtime.error ) {
-		fmessage.text( DATA.stops[STOP_INDEX].realtime.message );
-		ad(fmessage);
+		FMESSAGE.text( DATA.stops[STOP_INDEX].realtime.message );
+		ad(FMESSAGE);
 		return ;
 	}
 	
 	var n = DATA.stops[STOP_INDEX].realtime.results.length;
 	
 	if ( n === 0 ) {
-		fmessage.text( 'nothing right now' );
-		ad(fmessage);
+		FMESSAGE.text( 'nothing right now' );
+		ad(FMESSAGE);
 		return ;
 	}
 	
@@ -158,7 +198,7 @@ function _display ( ) {
 
 		var fline = new UI.Text({
 		 position: new Vector2(LEFT+37, 30+offset),
-		 size: new Vector2(w-91, 20),
+		 size: new Vector2(W-91, 20),
 		 font: 'gothic-24-bold',
 		 color: '#000000' ,
 		 textAlign: 'left',
@@ -167,7 +207,7 @@ function _display ( ) {
 		});
 
 		var fminutes = new UI.Text({
-		 position: new Vector2(LEFT+w-54,30+offset),
+		 position: new Vector2(LEFT+W-54,30+offset),
 		 size: new Vector2(22, 20),
 		 font: 'gothic-24-bold',
 		 text: next.minutes,
@@ -189,10 +229,7 @@ function _display ( ) {
 
 function other ( ) {
 	++STOP_INDEX ;
-	LINE_INDEX = 0 ;
-	if ( STOP_INDEX >= DATA.stops.length ) {
-		STOP_INDEX = 0 ;
-	}
+	if ( STOP_INDEX >= DATA.stops.length ) STOP_INDEX = 0 ;
 	_display ( ) ;
 }
 
@@ -200,18 +237,18 @@ function handle_error ( title , message ) {
 	console.log( 'handle_error:', title, message ) ;
 	if ( Date.now() - TIMESTAMP < TKO ) {
 		bindnav();
-		main.status('color', FOK);
-		main.status('backgroundColor', BOK);
+		MAIN.status('color', FOK);
+		MAIN.status('backgroundColor', BOK);
 		TIMEOUT = setTimeout( load , POLLRATE ) ;
 		return ;
 	}
-	main.status('color', FKO);
-	main.status('backgroundColor', BKO);
+	MAIN.status('color', FKO);
+	MAIN.status('backgroundColor', BKO);
 	clear();
-	fstop.text( title );
-	fmessage.text( message );
-	ad(fmessage);
-	ad(fstop);
+	FSTOP.text( title );
+	FMESSAGE.text( message );
+	ad(FSTOP);
+	ad(FMESSAGE);
 	bindload();
 }
 
@@ -220,42 +257,28 @@ function loadfail (data, status, request) {
 	handle_error('API failed ' + status , data.message ) ;
 }
 
-function loadsuccess (fg, bg) {
-	
-	return function (data, status, request) {
-		ERROR = null ;
-
-		DATA = data ;
-		STOP_INDEX = 0 ;
-		LINE_INDEX = 0 ;
-		if ( STOP_ID !== null ) {
-			var m = DATA.stops.length ;
-			for ( var i = 0 ; i < m ; ++i ) {
-				if ( DATA.stops[i].id === STOP_ID ) {
-					STOP_INDEX = i ;
-					if ( LINE_ID !== null ) {
-
-						var realtime = DATA.stops[i].realtime;
-
-						if ( realtime.error ) break ;
-						var results = realtime.results ;
-
-						var n = results.length ;
-						for ( var j = 0 ; j < n ; ++j ) {
-							if ( results[j].line === LINE_ID ) {
-								LINE_INDEX = j ;
-								break ;
-							}
-						}
-					}
-					break;
-				}
+function _update_stop_index(){
+	STOP_INDEX = 0 ;
+	if ( STOP_ID !== null ) {
+		var m = DATA.stops.length ;
+		for ( var i = 0 ; i < m ; ++i ) {
+			if ( DATA.stops[i].id === STOP_ID ) {
+				STOP_INDEX = i ;
+				break;
 			}
 		}
+	}
+}
+
+function loadsuccess (fg, bg) {
+	return function (data, status, request) {
+		ERROR = null ;
+		DATA = data ;
+		_update_stop_index() ;
 		_display();
 		bindnav();
-		main.status('color', fg);
-		main.status('backgroundColor', bg);
+		MAIN.status('color', fg);
+		MAIN.status('backgroundColor', bg);
 		TIMESTAMP = Date.now();
 		TIMEOUT = setTimeout( load , POLLRATE ) ;
 	} ;
@@ -274,8 +297,8 @@ function load ( ) {
 		TIMEOUT = null ;
 	}
 	
-	main.status('color', FLO ) ;
-	main.status('backgroundColor', BLO ) ;
+	MAIN.status('color', FLO ) ;
+	MAIN.status('backgroundColor', BLO ) ;
 	
 	var fg = FOK ;
 	var bg = BOK ;
@@ -292,12 +315,15 @@ function load ( ) {
 	ajax({ url: api(LAT,LON), type: 'json' }, loadsuccess( fg, bg ), loadfail ) ;
 }
 
+// geolocation
+
 function geosuccess ( position ) {
 	LAT = position.coords.latitude;
 	LON = position.coords.longitude;
 	console.log( 'geosuccess', LAT, LON ) ;
+	var failed_before = GEOERROR !== null ;
 	GEOERROR = null ;
-	if ( TIMEOUT === null ) load();
+	if ( TIMEOUT === null || failed_before ) load();
 }
 
 function geofail(){
@@ -337,10 +363,8 @@ function bindload ( ) {
 	unbind();
 	console.log('bindload'); 
 
-	main.on('click', 'select', function(e) { console.log('click'); load() ; } ) ;
-	main.on('longClick', 'select', function(e) { console.log('longClick'); load() ; } ) ;
-	//main.on('longClick', 'down', function(e) { load() ; } ) ;
-	//main.on('longClick', 'up', function(e) { load() ; } ) ;
+	MAIN.on('click', 'select', function(e) { console.log('click'); load() ; } ) ;
+	MAIN.on('longClick', 'select', function(e) { console.log('longClick'); load() ; } ) ;
 	
 	release();
 	
@@ -351,20 +375,19 @@ function bindnav ( ) {
 	unbind();
 	console.log('bindnav'); 
 	
-	main.on('click', 'select', function(e) { console.log('click'); other() ; } ) ;
-	main.on('longClick', 'select', function(e) { console.log('longClick'); load() ; } ) ;
-	//main.on('longClick', 'down', function(e) { load() ; } ) ;
-	//main.on('longClick', 'up', function(e) { load() ; } ) ;
+	MAIN.on('click', 'select', function(e) { console.log('click'); other() ; } ) ;
+	MAIN.on('longClick', 'select', function(e) { console.log('longClick'); load() ; } ) ;
 
-	main.on('hide', function(e){
+	MAIN.on('hide', function(e){
 		console.log('hide'); 
 		if ( TIMEOUT !== null ) {
 			clearTimeout(TIMEOUT);
 			TIMEOUT = null ;
 		}
+		freeze();
 	});
 
-	main.on('show', function(e){
+	MAIN.on('show', function(e){
 		console.log('show'); 
 		load();
 	});
@@ -374,10 +397,18 @@ function bindnav ( ) {
 
 function unbind ( ) {
 	console.log('unbind'); 	
-	try { main.off('click') ; }     catch ( e ) { }
-	try { main.off('longClick') ; } catch ( e ) { }
-	try { main.off('hide') ; }      catch ( e ) { }
-	try { main.off('show') ; }      catch ( e ) { }	
+	try { MAIN.off('click') ; }     catch ( e ) { }
+	try { MAIN.off('longClick') ; } catch ( e ) { }
+	try { MAIN.off('hide') ; }      catch ( e ) { }
+	try { MAIN.off('show') ; }      catch ( e ) { }	
 }
 
+// main
+
+unfreeze();
+if ( DATA !== null ) {
+	_update_stop_index();
+	_display();
+}
+if ( LAT !== null && LON !== null ) load();
 geostart();
